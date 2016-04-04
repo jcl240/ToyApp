b9d5c198a9bde43e53b9b2b98d7007b0f53a6dbf
Joseph <jcl240@georgetown.edu>
2016-04-04 12:48:32 -0400

    Initial commit

---
+++ app/src/main/java/programming/advanced/toyapp/checkoutActivity.java
@@ -0,0 +1,13 @@
+package programming.advanced.toyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class checkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkoutscreen);
+    }
+}
