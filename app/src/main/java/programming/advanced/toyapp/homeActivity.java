bf5eafc4d37dcf200fd6678b435fe6e3d02f5a67
Joseph <jcl240@georgetown.edu>
2016-04-06 11:33:19 -0400

    button functionality and reformatting

--- app/src/main/java/programming/advanced/toyapp/homeActivity.java
+++ app/src/main/java/programming/advanced/toyapp/homeActivity.java
@@ -1,7 +1,10 @@
 package programming.advanced.toyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homeActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class homeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
+
+    }
+
+    public void SearchForToys(View view)
+    {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homeActivity.this, searchActivity.class);
+                homeActivity.this.startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/programming/advanced/toyapp/searchActivity.java
+++ app/src/main/java/programming/advanced/toyapp/searchActivity.java
@@ -1,7 +1,9 @@
 package programming.advanced.toyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class searchActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class searchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchscreen);
+    }
+
+    public void checkOut(View view)
+    {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchActivity.this, checkoutActivity.class);
+                searchActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    public void cancel(View view)
+    {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchActivity.this, homeActivity.class);
+                searchActivity.this.startActivity(intent);
+            }
+        });
     }
 }
